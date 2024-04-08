672226d774b857fc99ab27312c76efa8fb34239f
wlarkspur <newacon@naver.com>
2024-04-08 23:12:43 +0200

    Router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+
 function App() {
-  return <div></div>;
+  const isLoggedIn = true;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
