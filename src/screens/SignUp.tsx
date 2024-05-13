ac83741c6300233d3870f2e3c0683b14612e1743
wlarkspur <newacon@naver.com>
2024-05-13 23:30:20 +0200

    Sign Up ing... useHistory(),useMuation(),useFrom()

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,6 +1,6 @@
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import AuthLayout from "../components/auth/AuthLayout";
 
@@ -13,6 +13,8 @@ import routes from "./routes";
 import { styled } from "styled-components";
 import { FatLink } from "../components/shared";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { gql, useMutation } from "@apollo/client";
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -26,7 +28,54 @@ const Subtitle = styled(FatLink)`
   margin-top: 10px;
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 function SignUp() {
+  const history = useHistory();
+  const onCompleted = (data: any) => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    history.push(routes.home);
+  };
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, formState } = useForm({
+    mode: "onChange",
+  });
+  const onSubmitValid = (data: any) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
   return (
     <AuthLayout>
       <Helmet>
@@ -39,12 +88,37 @@ function SignUp() {
             Sign up to see photos and videos from your friends
           </Subtitle>
         </HeaderContainer>
-        <form>
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Sign up" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            {...register("firstName", { required: "Firstname is required" })}
+            type="text"
+            placeholder="First Name"
+          />
+          <Input
+            {...register("lastName")}
+            type="text"
+            placeholder="Last Name"
+          />
+          <Input
+            {...register("username", { required: "Username is required" })}
+            type="text"
+            placeholder="Username"
+          />
+          <Input
+            {...register("email", { required: "Email is required" })}
+            type="text"
+            placeholder="Email"
+          />
+          <Input
+            {...register("password", { required: "Password is required" })}
+            type="password"
+            placeholder="Password"
+          />
+          <Button
+            type="submit"
+            value={loading ? "loading..." : "Sign Up"}
+            disabled={!formState.isValid || loading}
+          />
         </form>
       </FormBox>
       <BottomBox cta="Have an account?" link={routes.home} linkText="Log in" />
