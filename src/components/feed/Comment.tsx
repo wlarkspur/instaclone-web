0782526c94571dc2a6843a1ed5cabc1ef23b6256
wlarkspur <newacon@naver.com>
2024-05-21 22:45:37 +0200

    Hastags:React.Fragment/dangerouslysetHtml/

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,9 +1,20 @@
 import { styled } from "styled-components";
 import { FatText } from "../shared";
+import { Link } from "react-router-dom";
+import React from "react";
 
 const CommentContainer = styled.div``;
 export const CommentCaption = styled.span`
   margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accentColor};
+    cursor: pointer;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 interface IComment {
@@ -20,7 +31,19 @@ function Comment({ id, author, payload, children }: IComment) {
   return (
     <CommentContainer>
       <FatText>{author?.username}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption>
+        {payload?.split(" ").map((word, index) =>
+          /#[\w|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+/g.test(word) ? (
+            <React.Fragment key={index}>
+              <Link key={index} to={`/hashtags/${word}`}>
+                {word}{" "}
+              </Link>
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word}</React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 }
